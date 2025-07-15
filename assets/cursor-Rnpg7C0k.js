12e22c6493dcdbac2034a5b4c9fac3e592c16ccc
alleycat1 <netragonlong@gmail.com>
2025-07-15 05:07:13 +0000

    changed by feedback

---
+++ assets/cursor-Rnpg7C0k.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-DsEjwgpD.js";import"./index-xJv1Vh9Y.js";import"./index.es-DFeMIkhe.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
